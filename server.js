3ac023434dbd8d3e28fca4ea7bc5e1048a5a43f7
Dripshak <dripshakbachhar@gmail.com>
2026-09-23 13:20:43 +0545

    connect risk model module

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+const {
+  computeZones: computeZonesFromModel,
+} = require('./algorithms/risk');
+
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
