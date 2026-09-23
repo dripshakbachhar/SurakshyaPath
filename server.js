5c84b87a814cfdf2eb48484f59a98220cca2670b
Dripshak <dripshakbachhar@gmail.com>
2026-09-23 19:13:31 +0545

    refactor: use canonical zone configuration

--- server.js
+++ server.js
@@ -17,6 +17,7 @@
 
 const express = require('express');
 const fs = require('fs');
+const { ZONES } = require('./config/zones');
 const path = require('path');
 
 const {
@@ -40,78 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // CONFIGURATION
 // ============================================================================
 
-const ZONES = [
-  {
-    id: 'thamel',
-    name: 'Thamel',
-    np: 'ठमेल',
-    lat: 27.715,
-    lng: 85.312,
-  },
-  {
-    id: 'kalimati',
-    name: 'Kalimati',
-    np: 'कालीमाटी',
-    lat: 27.700,
-    lng: 85.282,
-  },
-  {
-    id: 'new-baneshwor',
-    name: 'New Baneshwor',
-    np: 'नयाँ बानेश्वर',
-    lat: 27.691,
-    lng: 85.335,
-  },
-  {
-    id: 'chabahil',
-    name: 'Chabahil',
-    np: 'चाबहिल',
-    lat: 27.717,
-    lng: 85.348,
-  },
-  {
-    id: 'koteshwor',
-    name: 'Koteshwor',
-    np: 'कोटेश्वर',
-    lat: 27.678,
-    lng: 85.348,
-  },
-  {
-    id: 'balaju',
-    name: 'Balaju',
-    np: 'बालाजु',
-    lat: 27.735,
-    lng: 85.303,
-  },
-  {
-    id: 'patan',
-    name: 'Patan',
-    np: 'पाटन',
-    lat: 27.673,
-    lng: 85.324,
-  },
-  {
-    id: 'gongabu',
-    name: 'Gongabu',
-    np: 'गोंगबु',
-    lat: 27.735,
-    lng: 85.313,
-  },
-  {
-    id: 'kirtipur',
-    name: 'Kirtipur',
-    np: 'कीर्तिपुर',
-    lat: 27.678,
-    lng: 85.277,
-  },
-  {
-    id: 'bouddha',
-    name: 'Bouddha',
-    np: 'बौद्ध',
-    lat: 27.721,
-    lng: 85.362,
-  },
-];
+
 
 const STATIONS = [
   {
